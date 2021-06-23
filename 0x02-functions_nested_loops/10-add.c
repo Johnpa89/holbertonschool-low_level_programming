35803a029ca8d60fb4e9504708cb2ac5093f9dc1
Johnpa89 <japaramo@hotmail.com>
2021-06-23 02:42:52 +0000

    0x02 - Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,9 +3,9 @@
  * add - a function that adds two integers and returns the result
  * @one: first number input
  * @two: second number input
- * Return: one + two
+ * Return: a + b
  */
-int add(int one, int two)
+int add(int a, int b)
 {
-	return (one + two);
+	return (a + b);
 }
