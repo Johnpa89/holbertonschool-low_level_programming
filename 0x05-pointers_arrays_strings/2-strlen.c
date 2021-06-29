55177f1f4970cf33762045c43f7f49c91dedf378
Johnpa89 <japaramo@hotmail.com>
2021-06-29 23:24:46 +0000

    project 0x05 - task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen - function to return lenght of a string
+ * @str: variable to read length
+ *
+ * Return - Lenght of str
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+		i++;
+	return (i);
+}
